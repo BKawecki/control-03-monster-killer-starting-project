2c0c303c7be1121447b61f628e5a2648078dfa7d
Bartek Kawecki <bkawecki21@gmail.com>
2023-02-20 23:24:56 +0100

    Changed printLogHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -82,15 +82,8 @@ function endRound() {
 }
 
 function attackMonster(mode) {
-    let maxDamage;
-    let logEvent;
-    if (mode === MODE_ATTACK) {
-       maxDamage = ATTACK_VALUE;
-       logEvent = LOG_EVENT_PLAYER_ATTACK;
-    } else if (mode === MODE_STRONG_ATTACK){
-        maxDamage = STRONG_ATTACK_VALUE;
-        logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-    }
+    const maxDamage = mode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+    const logEvent = mode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
     writeToLog(logEvent, damage, currentMonsterHealth, currentPlayerHealth);
@@ -120,7 +113,16 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-    console.log(battleLog);
+    let i = 0;
+    for (const battleLogElement of battleLog) {
+        console.log(`#${i}`);
+        i++;
+        for (const battleLogKey in battleLogElement) {
+            console.log(battleLogKey);
+            console.log(battleLogElement[battleLogKey]);
+        }
+        console.log('--------------------------------');
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
